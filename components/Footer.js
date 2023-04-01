71e5dcc401963b10cbf529fee398ec2dbe1ffa1a
Kelvin <kelvinpella@gmail.com>
2023-04-01 11:22:38 +0300

    footer added

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const footerItems = [
+    {
+      title: "Contact Us",
+      details: {
+        mobile: "+255 22 2170173",
+        email: "md@pizzaparadise.go.tz",
+      },
+    },
+    {
+      title: "Find our restaurants",
+      details: {
+        addresses: [
+          "Kinondoni Ave,2 Morogoro Road,Dar es Salaam",
+          "Mbezi Garden,Bagamoyo Road, Dar es Salaam",
+        ],
+      },
+    },
+    { title: "Working Hours", details: {time:'9:00 AM - 11:00 PM'} },
+  ];
   return (
-    <div className=''>Footer</div>
-  )
-}
+    <div className="w-full p-8 flex justify-end bg-bgFooter bg-center bg-cover">
+      {footerItems.map(({ title, details }) => {
+        const { mobile = null, email = null, addresses = null ,time=null} = details;
+        return (
+          <div key={title} className="m-6">
+            <h2 className="uppercase text-xl text-yellow-600 font-bold my-2">
+              {title}
+            </h2>
+            <div className="text-white font-bold">
+              {mobile && (
+                <p className="py-2">
+                  Mobile:{" "}
+                  <a
+                    href={`tel:${mobile}`}
+                    className="text-cyan-500 hover:text-cyan-400 font-bold"
+                  >
+                    {mobile}
+                  </a>
+                </p>
+              )}
+              {email && (
+                <p className="py-2">
+                  Email:{" "}
+                  <a
+                    href={`mailto:${email}`}
+                    className="text-blue-500 hover:text-blue-400 font-bold"
+                  >
+                    {email}
+                  </a>
+                </p>
+              )}
+              {time && (
+                <p className="py-2">
+                 {time}
+                </p>
+              )}
+              {addresses && (
+                <div>
+                  {addresses.map((address, i) => (
+                    <>
+                      <address key={i} className="whitespace-pre-line">
+                        {address.split(",").join("\n")}
+                      </address>
+                    </>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
       fontFamily: {
         dancingFont: ["Dancing Script", "cursive"],
       },
+      backgroundImage: {
+        bgFooter:
+          "linear-gradient(to right, transparent,#301111),url('https://images.unsplash.com/photo-1587899897387-091ebd01a6b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80')",
+      },
     },
   },
   plugins: [],
