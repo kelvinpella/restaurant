324d3cce01e51f273f30e4c50ef1d4124a11ca7e
Kelvin <kelvinpella@gmail.com>
2023-05-19 21:18:43 +0300

    removed link

--- components/Pizza.js
+++ components/Pizza.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 const Pizza = ({
   pizzaProps: {
     name,
@@ -9,24 +10,26 @@ const Pizza = ({
     coverimage: { alt, imageUrl },
   },
 }) => {
+  const router = useRouter();
+  const goToProductPage = () => {
+    router.push(`/product/${slug}`);
+  };
   return (
-    <div className="rounded-md p-2 text-center bg-slate-200 hover:bg-slate-300 cursor-pointer text-black ">
-      <Link href={`/product/${slug}`}>
-        {" "}
-        <div className="relative w-full h-32  py-2 md:py-4 ">
-          <Image src={imageUrl} fill alt={alt} className="object-contain " />
-        </div>
-        <h2 className="my-2 w-full text-red-800 font-bold md:text-lg">
-          {name}
-        </h2>
-        <p className="my-3 md:my-4 font-bold">
-          from{" "}
-          <span className="text-lg text-italic text-amber-800">${small}</span>
-        </p>
-        <p className="w-full  lg:w-10/12 text-sm md:text-base mx-auto my-2 md:my-4 text-black">
-          {about}
-        </p>
-      </Link>
+    <div
+      onClick={goToProductPage}
+      className="rounded-md p-2 text-center bg-slate-200 hover:bg-slate-300 cursor-pointer text-black "
+    >
+      <div className="relative w-full h-32  py-2 md:py-4 ">
+        <Image src={imageUrl} fill alt={alt} className="object-contain " />
+      </div>
+      <h2 className="my-2 w-full text-red-800 font-bold md:text-lg">{name}</h2>
+      <p className="my-3 md:my-4 font-bold">
+        from{" "}
+        <span className="text-lg text-italic text-amber-800">${small}</span>
+      </p>
+      <p className="w-full  lg:w-10/12 text-sm md:text-base mx-auto my-2 md:my-4 ">
+        {about}
+      </p>
     </div>
   );
 };
